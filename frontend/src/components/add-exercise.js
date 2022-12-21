38460a046261df30e6e217fe5d86267285550f6b
wrewsama <andrewlzs512@gmail.com>
2022-12-21 18:07:45 +0800

    Add modal footer buttons

--- frontend/src/components/add-exercise.js
+++ frontend/src/components/add-exercise.js
@@ -5,9 +5,14 @@ export default function AddExercise() {
     <div className="modal fade" id='add-exercise'>
         <div className="modal-dialog">
             <div className='modal-content'>
-                <div className='modal-header'>header</div>
-                <div className='modal-body'>body</div>
-                <div className='modal-footer'>footer</div>
+                <div className='modal-header'>Add a new exercise</div>
+                <div className='modal-body'>
+                    body
+                </div>
+                <div className='modal-footer'>
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                    <button type="button" class="btn btn-primary">Add</button>
+                </div>
             </div>
         </div>
     </div>
