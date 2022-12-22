be92eab1a7ca47a3336e05f418b276ecaa38c5ac
wrewsama <andrewlzs512@gmail.com>
2022-12-22 10:36:13 +0800

    add dropdown for edit and delete buttons

--- frontend/src/components/exercise-list-item.js
+++ frontend/src/components/exercise-list-item.js
@@ -3,9 +3,24 @@ import "bootstrap/dist/css/bootstrap.min.css"
 
 export default function ExerciseListItem({ exercise }) {
     return (
-        <button className='btn btn-light btn-block text-start'>
-            {exercise.name}
-        </button>
+        <div className="btn-group">
+            <button className='btn btn-light btn-block text-start'>
+                {exercise.name}
+            </button>
+
+            <div className="btn-group">
+                <button type="button"
+                        className="btn btn-light dropdown-toggle dropdown-toggle-split"
+                        data-bs-toggle="dropdown"
+                        aria-expanded="false"></button>
+                <ul className="dropdown-menu">
+                    <li><button className="dropdown-item">Edit</button></li>
+                    <li><hr className="dropdown-divider"></hr></li>
+                    <li><button className="dropdown-item">Delete</button></li>
+                </ul>
+                
+            </div>
+        </div>
     )
 }
 
