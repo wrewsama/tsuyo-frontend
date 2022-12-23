70fc29e938c136cb631ab0278d5a23d6e9e2fc73
wrewsama <andrewlzs512@gmail.com>
2022-12-23 13:58:01 +0800

    add WorkoutItem object

--- frontend/src/components/history.js
+++ frontend/src/components/history.js
@@ -1,9 +1,18 @@
 import React from 'react'
 
+// temporary set component
+const WorkoutItem = () => {
+    return <div>hi</div>
+}
+
 export default function History() {
     return (
         <div className="container">
-            History
+            <ul className="list-group">
+                <li className="list-group-item">
+                    <WorkoutItem />
+                </li>
+            </ul>
         </div>
     )
 }
