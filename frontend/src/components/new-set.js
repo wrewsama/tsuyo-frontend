ab1695ffad56e21d98bfdc09896187f64d05a5b3
wrewsama <andrewlzs512@gmail.com>
2022-12-23 10:24:08 +0800

    make disappearing edit and delete buttons

--- frontend/src/components/new-set.js
+++ frontend/src/components/new-set.js
@@ -1,25 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function NewSet({ idx, weight, reps }) {
+    const [style, setStyle] = useState({ display: 'none' })
+
+    const handleMouseEnter = event => {
+        setStyle({ display: 'block' })
+    }
+
+    const handleMouseLeave = event => {
+        setStyle({ display: 'none' })
+    }
+
     return (
-        <div className="container d-flex">
+        <div className="container d-flex" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
             <div className="container">
                 <div class='fw-bold'>Set {idx}</div>
                 {weight} kg x {reps} reps
             </div>
-            <div className="dropdown my-auto float-end">
-                <button className="btn dropdown-toggle" data-bs-toggle="dropdown"></button>
-                <ul className="dropdown-menu">
-                    <li>
-                        <button className="dropdown-item">
-                            Edit
-                        </button>
-                        <li><hr class="dropdown-divider"></hr></li>
-                        <button className="dropdown-item">
-                            Delete
-                        </button>
-                    </li>
-                </ul>
+            
+            <div className="btn-group btn-group-sm">
+                <button class="btn" style={style}>edit</button>
+                <button class="btn" style={style}>delete</button>
             </div>
             
         </div>
