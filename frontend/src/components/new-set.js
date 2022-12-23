b7967bc3f4e7280af9e4fb7e4071eafecf9259bb
wrewsama <andrewlzs512@gmail.com>
2022-12-23 10:12:25 +0800

    add dropdown for edit and delete newset

--- frontend/src/components/new-set.js
+++ frontend/src/components/new-set.js
@@ -2,8 +2,26 @@ import React from 'react'
 
 export default function NewSet({ idx, weight, reps }) {
     return (
-        <div>
-            {idx}. {weight} kg x {reps} reps
+        <div className="container d-flex">
+            <div className="container">
+                <div class='fw-bold'>Set {idx}</div>
+                {weight} kg x {reps} reps
+            </div>
+            <div className="dropdown my-auto float-end">
+                <button className="btn dropdown-toggle" data-bs-toggle="dropdown"></button>
+                <ul className="dropdown-menu">
+                    <li>
+                        <button className="dropdown-item">
+                            Edit
+                        </button>
+                        <li><hr class="dropdown-divider"></hr></li>
+                        <button className="dropdown-item">
+                            Delete
+                        </button>
+                    </li>
+                </ul>
+            </div>
+            
         </div>
     )
 }
