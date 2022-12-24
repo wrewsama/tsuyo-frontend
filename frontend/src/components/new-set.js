fc8fc027ac17284bfe8a0eb5d2c72256cb8190d4
wrewsama <andrewlzs512@gmail.com>
2022-12-24 23:46:49 +0800

    Add comments for new set

--- frontend/src/components/new-set.js
+++ frontend/src/components/new-set.js
@@ -1,17 +1,37 @@
 import React, { useState } from 'react'
 import EditNewSet from './edit-new-set'
 
+/**
+ * Container for the information and functionality for a given New Set.
+ * 
+ * @param {Number} idx The index number of this New Set.
+ * @param {Number} weight The weight used for this New Set.
+ * @param {Number} reps The number of reps performed in this New Set.
+ * @param {Function} deleteFunction A callback function to delete this New Set
+ *                                  from the list in the New page
+ * @param {Function} editFunction A callback function to edit this New Set
+ *                                and update the list in the New page
+ */
 export default function NewSet({ idx, weight, reps, deleteFunction, editFunction }) {
     const [style, setStyle] = useState({ display: 'none' })
 
+    /**
+     * Makes certain divs visible when the mouse enters an area.
+     */
     const handleMouseEnter = event => {
         setStyle({ display: 'block' })
     }
 
+    /**
+     * Makes certain divs disappear when the mouse leaves an area.
+     */
     const handleMouseLeave = event => {
         setStyle({ display: 'none' })
     }
 
+    /**
+     * Deletes this New Set when the delete button is clicked.
+     */
     const onDeleteButtonClick = event => {
         deleteFunction(idx)
     }
