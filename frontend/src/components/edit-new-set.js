7321bbc6402a990657d4cbdb60d76766045e77a1
wrewsama <andrewlzs512@gmail.com>
2022-12-24 22:32:54 +0800

    Add comments for edit-new-set

--- frontend/src/components/edit-new-set.js
+++ frontend/src/components/edit-new-set.js
@@ -3,22 +3,38 @@ import React, { useState } from 'react'
 /**
  * Modal to facilitate the editing of a New Set in the New page.
  * 
- * @param {Object} idx
- * @returns 
+ * @param {Number} idx The index of the New Set to be edited.
+ * @param {Number} initWeight The original weight in the New Set.
+ * @param {Number} initReps The original number of reps in the New Set.
+ * @param {Function} editFunction A callback function to update the New Set in
+ *                                the New component.
  */
 export default function EditNewSet({ idx, initWeight, initReps, editFunction }) {
     const [submitted, setSubmitted] = useState(false)
     const [weight, setWeight] = useState(initWeight)
     const [reps, setReps] = useState(initReps)
 
+    /**
+     * Updates the weight state when the weight input is changed by the user.
+     */
     const handleWeightChange = event => {
         setWeight(event.target.value)
     }
 
+    /**
+     * Updates the reps state when the reps input is changed by the user.
+     */
     const handleRepChange = event => {
         setReps(event.target.value)
     }
 
+    /**
+     * Edits the New Set.
+     * 
+     * When the Save Button is clicked, take the weight and reps from their
+     * respective states and pass them into the editFunction. Then update
+     * the submitted state to true.
+     */
     const onSaveButtonClick = event => {
         editFunction(idx, weight, reps)
         setSubmitted(true)
