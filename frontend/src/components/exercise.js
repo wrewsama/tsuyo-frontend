7015d9a6cd002291f950bcfa7a0f0caabe1c2660
wrewsama <andrewlzs512@gmail.com>
2022-12-24 22:49:25 +0800

    Add comments for exercise

--- frontend/src/components/exercise.js
+++ frontend/src/components/exercise.js
@@ -2,10 +2,25 @@ import React, { useState, useEffect } from 'react'
 import { useParams, Link, Outlet } from 'react-router-dom'
 import DataService from '../services/exercise'
 
+/**
+ * Layout for the /:id route.
+ * 
+ * Includes the Exercise's name as a header, as well as a navbar allowing
+ * for navigation between the New, Header, and Graph sections. Also passes
+ * the exercise object as the Outlet context.
+ */
 export default function Exercise() {
     const [exercise, setExercise] = useState({})
     const { id } = useParams()
 
+    /**
+     * Sets the exercise state to the correct exercise object.
+     * 
+     * Gets the id from the url params, then sends a http get request with
+     * that id to get the corresponding Exercise data from the database.
+     * Then update the exercise state using that data. This is called
+     * the first time the page is rendered.
+     */
     useEffect(() => {
         DataService.findExerciseById(id)
         .then(res => {
