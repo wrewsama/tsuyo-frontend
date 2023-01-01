d9f75c97796104741c7955c0c0afe92c5aae0015
wrewsama <andrewlzs512@gmail.com>
2023-01-01 20:49:51 +0800

    add conditionals to navbar

--- frontend/src/components/main-navbar.js
+++ frontend/src/components/main-navbar.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { useLogout } from "../hooks/useLogout";
+import { useAuthContext } from '../hooks/useAuthContext';
 
 export default function MainNavbar() {
     const { logout } = useLogout()
+    const { user } = useAuthContext()
 
     const handleLogout = () => {
         logout()
@@ -12,22 +14,41 @@ export default function MainNavbar() {
     return (
         <nav className="navbar navbar-expand">
             <div className="container">
-            <Link to='/' className = "navbar-brand"> tsuyo </Link>
+                <Link to='/' className = "navbar-brand"> tsuyo </Link>
 
-            <ul className="navbar-nav">
-                <li className="nav-item">
-                <button className="btn"
-                        onClick={handleLogout}>
-                    Log out
-                </button>
-                </li>
-                <li className="nav-item">
-                <Link to="/login" className="nav-link">Log in</Link>
-                </li>
-                <li className="nav-item">
-                <Link to="/signup" className="nav-link">Sign Up</Link>
-                </li>
-            </ul>
+                {
+                    user ? (
+                        <div className="container">
+                            <ul className="navbar-nav">
+                                <li className="navbar-text ms-auto">
+                                    {user.email}
+                                </li>
+                                <li className="nav-item">
+                                    <button className="btn"
+                                            onClick={handleLogout}>
+                                        Log out
+                                    </button>
+                                </li>
+                            </ul>
+                            
+                        </div>
+                    ) : (
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <button className="btn"
+                                        onClick={handleLogout}>
+                                    Log out
+                                </button>
+                            </li>
+                            <li className="nav-item">
+                                <Link to="/login" className="nav-link">Log in</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to="/signup" className="nav-link">Sign Up</Link>
+                            </li>
+                        </ul>
+                    )
+                }
             </div>
         </nav>
     )
