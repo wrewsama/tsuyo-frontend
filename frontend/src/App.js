dd0ecd1fd89e5209e482a0ed4e3c51ea1c82883f
wrewsama <andrewlzs512@gmail.com>
2023-01-02 18:51:08 +0800

    protect routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route, Navigate } from "react-router-dom"
 import "bootstrap/dist/css/bootstrap.min.css"
 import ExercisesList from './components/exercises-list'
 import Exercise from "./components/exercise";
@@ -9,21 +9,33 @@ import Graph from "./components/graph";
 import Login from "./components/login";
 import Signup from "./components/signup";
 import MainNavbar from "./components/main-navbar";
+import { useAuthContext } from "./hooks/useAuthContext";
 
 const App = () => {
+  const{ user } = useAuthContext()
+
   return (
     <div>
       <MainNavbar />
           
       <Routes>
-        <Route path="/" element={<ExercisesList />} />
-        <Route path="/:id" element={<Exercise />}>
+        <Route path="/"
+               element={user
+                        ? <ExercisesList />
+                        : <Navigate to="/login" />} />
+        <Route path="/:id" element={user
+                                    ? <Exercise />
+                                    : <Navigate to="/login" />}>
           <Route index element={<New />} />
           <Route path="history" element={<History />} />
           <Route path="graph" element={<Graph />} />
         </Route>
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={!user
+                                      ? <Login />
+                                      : <Navigate to="/" />} />
+        <Route path="/signup" element={!user
+                                       ? <Signup />
+                                       : <Navigate to="/" />} />
       </Routes>
     </div>
     
